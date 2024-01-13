8f034600a46d7cb060ff050f9b87c97dc9467612
stevesdiary <stephenoyeyemi@gmail.com>
2024-01-13 01:36:27 +0100

    Fetch weather data successfully

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const env = require('dotenv');
+const port = 3000;
+const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/weather', (req, res) => {
-  try{
-    const city =  req.query.city;
-    const units  = req.query.units;
-    const fetchWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${preocess.env.WEATHER_API_KEY}&units${units}`;
-    if(fetchWeather !== 0){
-      return res.status(200).send({Message: `Here is what the weather is in ${city}`, Weather: fetchWeather})
+app.post('/', async (req, res) => {
+  try {
+    const city = req.query.city;
+    const units = req.query.units;
+    const API_KEY = '40a8904c86de719133c8841b56287066';
+    const fetchWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+    const weatherData = await fetch(fetchWeather);
+    const jsonData = await weatherData.json();
+    if (weatherData !== 0) {
+      return res.status(200).send({ Message: `Here is what the weather is in ${city}`, Weather: jsonData });
     }
-    
-  }
-  catch(err){
-    return res.status(500).send({Message: `Unable to fetch data for ${city}, an error occoured`, Error: err});
+
+    return res.send({ Message: `Something went wrong!` });
+  } catch (err) {
+    return res.status(500).send({ Message: `Unable to fetch data for ${city}, an error occurred`, Error: err });
   }
 });
 
-app.listen(port, (req, res) => {
-  
-})
+app.listen(port, async() => {
+  console.log(`App running on port ${port}`)
+});
